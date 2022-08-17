5c0d37234435db9e540d0fec71d58a85ee22e87a
SINIOR22 <106260846+SINIOR22@users.noreply.github.com>
2022-08-17 17:26:50 +0300

    "master"

--- js/staly.js
+++ js/staly.js
@@ -1,12 +1,12 @@
 const acc = document.getElementsByClassName("accordion-item-title");
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function()  {
-    this.classList.toggle("active");
+    this.classList.toggle("block");
     const panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
       panel.style.display = "block";
     }
-})
+}) 
 }
